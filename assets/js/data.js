7db3f0552e850cae8ef4409489d546c58e8231ce
kratos <99797338+KratosRepo@users.noreply.github.com>
2024-01-07 20:29:49 +0530

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,12 @@
+let movies = [
+  {
+    name: "IND vs SA",
+    des: "India and South Africa draw the series...!!!",
+    image: "https://www.mykhel.com/img/2024/01/ind-vs-sa-2nd-test-cape-town-1200-1704372825.jpg",
+  },
+  {
+    name: "IND vs SA",
+    des: "Virat Kohli is a mood.",
+    image: "https://pbs.twimg.com/media/GDABSC5bwAARYQ0?format=jpg&name=900x900",
+  },
+];
