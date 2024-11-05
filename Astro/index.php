5f71f3176572b650c5cf54887b06cb7976aa129b
sports-exclusive <155051262+sports-exclusive@users.noreply.github.com>
2024-11-05 18:12:50 +0530

    Update

---
+++ Astro/index.php
@@ -0,0 +1,15 @@
+<?php
+$get = $_GET['get'];
+$mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/' . $get;
+
+$mpdheads = [
+  'http' => [
+      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+$res = file_get_contents($mpdUrl, false, $context);
+echo $res;
+?>
